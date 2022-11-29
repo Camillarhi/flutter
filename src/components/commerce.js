470d6e80d74859175a80a42981d25b5ec0feff83
Camillarhi <rhitanene@gmail.com>
2022-11-30 00:27:56 +0100

    commerce component

--- src/components/commerce.js
+++ src/components/commerce.js
@@ -5,16 +5,21 @@ import Button from './button';
 const CommerceText = styled.h1`
 font-size:80px;
 `;
+
+const StrengthText = styled.p`
+font-size:75px;
+`;
+
 export default function Commerce() {
     return (
-        <div className='bg-secondary '>
-            <div className='container pt-5'>
+        <div className='bg-secondary pb-5'>
+            <div className='container py-5'>
                 <CommerceText className='pt-5 fw-bolder'>
                     Build a successful online business with our <br />
                     <span className='text-success'>Commerce tools.</span>
                 </CommerceText>
 
-                <div className='d-flex justify-content-between flex-column'>
+                <div className='d-flex justify-content-between flex-column pb-5 mb-5'>
                     <div className='d-flex justify-content-between'>
                         <div className='d-flex align-items-center'>
                             <div>
@@ -36,6 +41,8 @@ export default function Commerce() {
                     <div className='d-flex justify-content-between'>
                         <div className='w-50'>
                             <img className='img-fluid' src='https://cdn.filestackcontent.com/resize=width:700/output=format:webp/lc5K7jXVQiS9jBWmcdKg' alt='stack' />
+                            <p className='mt-4'>A platform for you, whether you’re a big business or a
+                                <span className='text-success'> solo artist.</span></p>
                         </div>
                         <div className="d-flex justify-content-around flex-column py-5 w-50">
                             <div className='ms-5 ps-5'>
@@ -63,6 +70,20 @@ export default function Commerce() {
                             </div>
                         </div>
                     </div>
+                </div>
+
+                <div className='py-5'>
+                    <StrengthText className='pb-4'>
+                        Let’s play to our strengths,<br />
+                        shall we? You focus on bringing<br />
+                        your amazing ideas to life and<br />
+                        we focus on providing all the<br />
+                        <span className='text-success'> commerce tools</span> you need.
+                    </StrengthText>
+                    <Button 
+                    text="Okay, let’s go"
+                    buttonClass="dark px-4 py-3"
+                    />
                 </div>
             </div>
 
