03b231fb52b44a6aa7f8372753d81147180369d0
Camillarhi <rhitanene@gmail.com>
2022-12-01 13:14:12 +0100

    code clean up

--- src/components/banner.js
+++ src/components/banner.js
@@ -12,7 +12,7 @@ export default function Banner() {
         <div className='container'>
             <div className='pt-5'>
                 <BannerText className='fw-bolder'>Endless <br />
-                    possibility<span className='text-dark'> for</span>
+                    possibilities<span className='text-dark'> for</span>
                 </BannerText>
                 <BannerText className='fw-bolder text-dark'>every business</BannerText>
             </div>
